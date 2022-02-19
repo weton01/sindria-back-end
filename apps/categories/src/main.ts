af52a60dededd73c0b335d28662e587e34d9c6cc
Wellington Campana Ferreira <wellington_ferreira280@hotmail.com>
2022-02-18 21:38:24 -0300

    feat: delete categories module

--- apps/categories/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { CategoriesModule } from './categories.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(CategoriesModule);
-  await app.listen(3000);
-}
-bootstrap();
