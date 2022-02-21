0673336251537ebae78908bc07e37b03e7563575
Cyber-SubFlame <wesley9983@gmail.com>
2022-02-21 10:36:46 -0300

    fix: teacher

--- apps/teacher/src/factories/app.module.ts
+++ apps/teacher/src/factories/app.module.ts
@@ -1,25 +1,11 @@
-import { SkillEntity, UserEntity } from '@app/common';
-import { TeacherEntity } from '@app/common/entities/teacher.entity';
-import { envs } from '@app/utils';
+import { TypeormConfig } from '@app/common';
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { FormationModule } from './formation.module';
 import { SkillModule } from './skill.module';
 import { TeacherModule } from './teacher.module';
 
 @Module({
-  imports: [
-    FormationModule,
-    TeacherModule,
-    SkillModule,
-    TypeOrmModule.forRoot({
-      type: 'mongodb',
-      url: `${envs.MONGO_URI}`,
-      entities: [TeacherEntity, UserEntity, SkillEntity],
-      ssl: true,
-      authSource: 'admin',
-    }),
-  ],
+  imports: [FormationModule, TeacherModule, SkillModule, TypeormConfig],
   controllers: [],
   providers: [],
 })
