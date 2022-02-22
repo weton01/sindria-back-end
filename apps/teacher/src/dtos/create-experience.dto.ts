3292fe6ab866ac054a97b44a5b33c3ecb206166b
Cyber-SubFlame <wesley9983@gmail.com>
2022-02-22 17:52:26 -0300

    feat: add experience

--- apps/teacher/src/dtos/create-experience.dto.ts
+++ apps/teacher/src/dtos/create-experience.dto.ts
@@ -3,14 +3,14 @@ import { Type } from 'class-transformer';
 import { IsString, IsNotEmpty, IsOptional, ValidateNested } from 'class-validator';
 import { RangeSchemaDto } from './range-schema.dto';
 
-export class CreateFormationDto {  
+export class CreateExperienceDto {  
   @IsString()
   @IsNotEmpty()
   name: string;
  
   @IsOptional()
   teacher: TeacherEntity;
-
+ 
   @ValidateNested( )
   @Type(() => RangeSchemaDto)
   range: RangeSchemaDto
--- libs/common/src/entities/index.ts
+++ libs/common/src/entities/index.ts
@@ -2,4 +2,5 @@ export * from './user.entity';
 export * from './teacher.entity'
 export * from './skill.entity';
 export * from './formation.entity';
-export * from './rangeSchema.entity'
+export * from './experience.entity';
+export * from './rangeSchema.entity';
