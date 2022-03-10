b5ece33e2fef544dae3c4d2c1ee6698edbc1f486
Wellington Campana Ferreira <wellington_ferreira280@hotmail.com>
2022-03-10 00:42:28 -0300

    updates

--- apps/teacher/src/dtos/delete-skill.dto.ts
+++ apps/teacher/src/dtos/delete-skill.dto.ts
@@ -2,5 +2,5 @@ import { IsString, IsNotEmpty } from 'class-validator';
 
 export class DeleteSkillDto {
   @IsString()
-  _id: string;
+  id: string;
 }
--- apps/teacher/src/dtos/get-by-id-teacher.dto.ts
+++ apps/teacher/src/dtos/get-by-id-teacher.dto.ts
@@ -3,5 +3,5 @@ import { IsString, IsNotEmpty } from 'class-validator';
 export class GetByIdTeacherDto {
   @IsString()
   @IsNotEmpty()
-  _id: string;
+  id: string;
 }
